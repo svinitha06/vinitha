06bd05f2ae67bf04a6aa04bd667458735a0be939
svinith06 <20072008smanoj@gmail.com>
2021-01-22 14:40:23 +0530

    commit maven

---
+++ src/main/java/com/sum/App.java
@@ -0,0 +1,19 @@
+package com.sum;
+import java.util.*;
+import java.io.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+         int c=a+b;
+          System.out.println("The sum is "+c);
+    }
+}
